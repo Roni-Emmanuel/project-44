7d7ded6d0a290ca6112d1cf846d43c0e72cd2444
Roni-Emmanuel <74718809+Roni-Emmanuel@users.noreply.github.com>
2021-12-29 16:43:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+var ship
+var chennai
+var rock1,rock2,rock3,rock4,rock5,rock6,ship;
+
+function setup() {
+  createCanvas(800,400);
+  ship = createSprite(400, 200, 50, 50);
+  
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
